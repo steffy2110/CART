608ac7f0ee05944ea3f2c8c03264c82e1a37360d
Steffy Varghese <steffy.varghese@capgemini.com>
2020-04-24 03:59:17 +0530

    components creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/home/home.component';
+import { ProductsComponent } from './modules/products/products.component';
+import { NoPageFoundComponent } from './modules/no-page-found/no-page-found.component';
+import { SuccessComponent } from './modules/success/success.component';
+import { ErrorComponent } from './modules/error/error.component';
+import { LoginComponent } from './modules/login/login.component';
+import { NavbarComponent } from './modules/navbar/navbar.component';
+import { FooterComponent } from './modules/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    NoPageFoundComponent,
+    SuccessComponent,
+    ErrorComponent,
+    LoginComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
